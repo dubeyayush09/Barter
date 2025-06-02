fccd5dff6633b668a7237b29eb2269539c6976f3
Ayush <dubeyayush09@gmail.com>
2025-06-02 12:43:00 +0530

    url changed

--- Backend/server.js
+++ Backend/server.js
@@ -25,6 +25,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "https://barter-chi.vercel.app", // your frontend domain
+    credentials: true, // if you're using cookies or sessions
+  })
+);
 
 // MongoDB connection
 mongoose
